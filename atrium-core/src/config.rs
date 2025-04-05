e68c76c404b3837bf56abe3627d12e177e58f872
leiysky <leiysky@outlook.com>
2025-04-06 01:09:54 +0800

    support config file

--- atrium-core/src/config.rs
+++ atrium-core/src/config.rs
@@ -1,1 +1,25 @@
+use serde::Deserialize;
+use serde::Serialize;
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Config {
+    #[serde(default = "default_listen_addr")]
+    pub listen_addr: String,
+    #[serde(default = "default_advertise_addr")]
+    pub advertise_addr: String,
+    #[serde(default = "default_path")]
+    pub path: String,
+    pub capacity: u64,
+}
+
+fn default_listen_addr() -> String {
+    "0.0.0.0:7654".to_string()
+}
+
+fn default_advertise_addr() -> String {
+    "127.0.0.1:7654".to_string()
+}
+
+fn default_path() -> String {
+    "/usr/local/atrium".to_string()
+}
