ad4e2eb3ad5fcaf97108a7e7a678f66e2d528679
Yu Lei <leiysky@outlook.com>
2025-04-06 01:10:36 +0800

    chore: support config file (#2)
    * support config file
    
    * tail line

--- atrium-core/src/main.rs
+++ atrium-core/src/main.rs
@@ -1,5 +1,16 @@
+use clap::Parser;
+
+use crate::config::Config;
+
+mod config;
 mod engine;
 mod server;
+
+#[derive(clap::Parser)]
+struct Command {
+    #[clap(short, long, help = "Path to config file")]
+    config: String,
+}
 
 #[tokio::main]
 async fn main() {
@@ -9,6 +20,11 @@ async fn main() {
                 .append(logforth::append::Stdout::default())
         })
         .apply();
+
+    let cmd = Command::parse();
+    let config = toml::from_str::<Config>(&std::fs::read_to_string(&cmd.config).unwrap()).unwrap();
+
+    log::info!("config: {config:#?}");
 
     let _ = server::start_server().await.inspect_err(|err| {
         log::error!("server stopped: {}", err);
