7d2ec1036e0036784f8281f9d85043211a3fdbc5
tison <wander4096@gmail.com>
2025-09-24 08:53:42 +0800

    merge tests for simplicity for now
    Signed-off-by: tison <wander4096@gmail.com>

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -13,9 +13,12 @@
 // limitations under the License.
 
 use std::any::Any;
+use std::process::ExitCode;
 use std::sync::Arc;
 
 use mea::shutdown::ShutdownSend;
+use percas_client::Client;
+use percas_client::ClientFactory;
 use percas_core::Config;
 use percas_core::FoyerEngine;
 use percas_core::LogsConfig;
@@ -23,11 +26,12 @@ use percas_core::Runtime;
 use percas_core::ServerConfig;
 use percas_core::StorageConfig;
 use percas_core::TelemetryConfig;
+use percas_core::make_runtime;
 use percas_server::server::ServerState;
 use percas_server::server::make_acceptor_and_advertise_addr;
 use percas_server::telemetry;
 
-pub fn make_test_name<TestFn>() -> String {
+fn make_test_name<TestFn>() -> String {
     let replacer = regex::Regex::new(r"[^a-zA-Z0-9]").unwrap();
     let test_name = std::any::type_name::<TestFn>()
         .rsplit("::")
@@ -39,7 +43,7 @@ pub fn make_test_name<TestFn>() -> String {
 type DropGuard = Box<dyn Any>;
 
 #[derive(Debug)]
-pub struct TestServerState {
+struct TestServerState {
     pub server_state: ServerState,
     shutdown_tx: ShutdownSend,
     _drop_guards: Vec<DropGuard>,
@@ -52,8 +56,8 @@ impl TestServerState {
     }
 }
 
-pub fn start_test_server(test_name: &str, rt: &Runtime) -> Option<TestServerState> {
-    let service_name = format!("testkit:harness:{test_name}").leak();
+fn start_test_server(test_name: &str, rt: &Runtime) -> Option<TestServerState> {
+    let service_name = format!("test_harness:{test_name}").leak();
 
     let mut drop_guard = Vec::<DropGuard>::new();
     drop_guard.extend(
@@ -128,4 +132,44 @@ pub fn start_test_server(test_name: &str, rt: &Runtime) -> Option<TestServerStat
         shutdown_tx,
         _drop_guards: drop_guard,
     })
+}
+
+pub struct Testkit {
+    pub client: Client,
+}
+
+pub fn harness<T, Fut>(test: impl Send + FnOnce(Testkit) -> Fut) -> ExitCode
+where
+    T: std::process::Termination,
+    Fut: Send + Future<Output = T>,
+{
+    let rt = make_runtime("test_runtime", "test_thread", 4);
+
+    let test_name = make_test_name::<Fut>();
+    let Some(state) = start_test_server(&test_name, &rt) else {
+        return ExitCode::SUCCESS;
+    };
+
+    rt.block_on(async move {
+        let server_addr = format!("http://{}", state.server_state.advertise_addr());
+        let factory = ClientFactory::new().unwrap();
+        let client = factory.make_client(server_addr).unwrap();
+
+        let exit_code = test(Testkit { client }).await.report();
+
+        state.shutdown().await;
+        exit_code
+    })
+}
+
+pub fn render_hex<T: AsRef<[u8]>>(data: T) -> String {
+    let config = pretty_hex::HexConfig {
+        width: 8,
+        group: 0,
+        ..Default::default()
+    };
+    format!(
+        "{:?}",
+        pretty_hex::PrettyHex::hex_conf(&data.as_ref(), config)
+    )
 }
